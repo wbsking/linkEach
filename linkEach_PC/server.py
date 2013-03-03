ffed609cd493fd4038f04fa0ba2c678523a1c69e
sking <wbsking@gmail.com>
2013-03-03 17:26:43 +0800

    add files

---
+++ linkEach_PC/server.py
@@ -0,0 +1,18 @@
+import logging
+import socket
+
+import consts
+import broadcast
+
+class Server(object):
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+    
+    
+        
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
