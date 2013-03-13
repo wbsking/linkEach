8479f496af892688bd972a48f97c96b7338d3c27
sking <wbsking@gmail.com>
2013-03-13 19:38:18 +0800

    create main widget

--- linkEach_PC/main.py
+++ linkEach_PC/main.py
@@ -24,11 +24,10 @@ class mainWindow(QtGui.QWidget):
         self.setWindowTitle('linkEach')
         self.set_center()
         
-        self.create_cast_group_layout()
-        self.setLayout(self.cast_group_layout)
         self.create_refresh_wdiget()
         self.setStyleSheet("background-color:#00ff00")
-        self.show()
+        self.add_cast_label('test1')
+#         self.add_cast_label('test2')
     
     def set_center(self):
         screen = QtGui.QDesktopWidget().screenGeometry()
@@ -36,23 +35,34 @@ class mainWindow(QtGui.QWidget):
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
 
     
-    def create_cast_group_layout(self):
-        self.cast_group_layout = QtGui.QGridLayout()
+    def add_cast_label(self, name='test2'):
+        print 11111111111111111111111111111
+        label = QtGui.QLabel(name, self)
+        label.setFrameShape(QtGui.QFrame.StyledPanel)
+        label.setStyleSheet("background-color:rgb(200, 200, 200);"
+                            "border:1px solid gray;")
+        label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.ani = QtCore.QPropertyAnimation(label, 'geometry')
+        self.ani.setDuration(500)
+        self.ani.setStartValue(QtCore.QRect(0, 400, 300, 50))
+        self.ani.setEndValue(QtCore.QRect(0, 50*len(self.cast_labels), 300, 50))
+#         label.setGeometry(0, 50*len(self.cast_labels), 300, 50)
+        self.cast_labels.append(label)
+        self.ani.start()
         
-    def add_cast_label(self, name):
-        label = QtGui.QLabel(name)
         
     def create_refresh_wdiget(self):
         self.refresh_label = QtGui.QLabel('Refresh', self)
         self.refresh_label.setStyleSheet("background-color:#0000ff")
         self.refresh_label.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
         self.refresh_label.setGeometry(0, 350, 300, 50)
-        
+        self.refresh_label.connect(self, QtCore.SIGNAL('clicked'), self.add_cast_label)
           
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
     wind = mainWindow()
+    wind.show()
     sys.exit(app.exec_())
     
     
