9243901d2865a8512f12340dfc8432af7dc5870a
sking <wbsking@gmail.com>
2013-04-09 22:32:42 +0800

    add animation for label

--- linkEach_Android/gen/com/example/linkeach/R.java
+++ linkEach_Android/gen/com/example/linkeach/R.java
@@ -8,25 +8,28 @@
 package com.example.linkeach;
 
 public final class R {
+    public static final class anim {
+        public static final int trans=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int loading_bar=0x7f070001;
-        public static final int main_layout=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int loading_bar=0x7f080001;
+        public static final int main_layout=0x7f080000;
+        public static final int menu_settings=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int menu_settings=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int menu_settings=0x7f050001;
     }
     public static final class style {
         /** 
@@ -50,11 +53,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
-        public static final int cast_label=0x7f050002;
+        public static final int AppTheme=0x7f060001;
+        public static final int cast_label=0x7f060002;
     }
 }
--- linkEach_Android/src/com/example/linkeach/MainActivity.java
+++ linkEach_Android/src/com/example/linkeach/MainActivity.java
@@ -9,14 +9,20 @@ import android.os.Message;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.ProgressBar;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener{
 	public List cast_label_list;
 	public Map cast_map;
 	public ProgressBar loading_bar;
@@ -51,19 +57,43 @@ public class MainActivity extends Activity {
 	public void add_cast_label(Entry<String, Map> entry){
 		String client_ip = entry.getKey();
 		Map<String, String> tmp_map = entry.getValue();
-		
-		cast_label.setBackgroundColor(Color.rgb(120, 120, 120));
+		String name = tmp_map.get("name");
+		cast_label.setBackgroundColor(Color.rgb(0, 178, 238));
 		cast_label.setText(client_ip);
-		cast_label.setHeight(50);
-		
+		cast_label.setHeight(100);
+		cast_label.setTextSize(20);
+		cast_label.setGravity(Gravity.CENTER);
 		cast_label.setVisibility(View.VISIBLE);
+		cast_label.setClickable(true);
+		Animation ani = AnimationUtils.loadAnimation(this, R.anim.trans);
 		
-		Map tmp = new HashMap<String, TextView>();
+		cast_label.setAnimation(ani);
+		
+		Map tmp = new HashMap<String, Object>();
 		tmp.put("label", cast_label);
-		cast_label_list.add(tmp);
+		tmp.put("name", name);
+		Map label_tmp = new HashMap<String, Map>();
+		label_tmp.put(client_ip, tmp);
+		cast_label_list.add(label_tmp);
+		
+		cast_label.setOnTouchListener(this);
+		
 		main_layout = (RelativeLayout)findViewById(R.id.main_layout);
-		main_layout.addView(cast_label);
+		main_layout.addView(cast_label, new LayoutParams(LayoutParams.MATCH_PARENT, 
+				LayoutParams.WRAP_CONTENT));
 	}
+	
+	public boolean onTouch(View v, MotionEvent event){
+		if(MotionEvent.ACTION_DOWN == event.getAction()){
+			v.setBackgroundColor(Color.rgb(0, 153, 204));
+		}
+		else if(MotionEvent.ACTION_UP == event.getAction()){
+			cast_label.setBackgroundColor(Color.rgb(0, 178, 238));
+		}
+		return false;
+		}
+	
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -113,7 +143,6 @@ public class MainActivity extends Activity {
 			if(msg!=null){
 				MainActivity.this.hide_loading();
 				Map<String, Map> remote_clients = (Map)msg.getData().getSerializable("map");
-				System.out.println(remote_clients);
 				for(Map.Entry<String, Map> entry:remote_clients.entrySet()){
 					String client_ip = entry.getKey();
 					int i;
